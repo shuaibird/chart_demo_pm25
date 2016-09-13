97206507edc15bcd06b2bca5fdc574bc5e6c4557
Shuaibird <shuaibird@icloud.com>
2016-09-13 11:13:56 +0800

    express + superagent

---
+++ server.js
@@ -0,0 +1,15 @@
+const HOST = 'http://119.29.166.125:8080'
+
+var express = require('express')
+var app = express()
+var request = require('superagent')
+
+app.use(express.static('public'))
+
+request
+  .get(`${HOST}/dataapi/view/v2/AVZa1KdFwe2Jxq0nEb01?data={"PRODUCT_KEY":"pk1","DID":"did1","FROM_TIMESTAMP_MILLIS":1473468218123,"TO_TIMESTAMP_MILLIS":1473557618123}`)
+  .end((err, res) => {
+    console.log(res.body)
+  })
+
+app.listen(8080);
