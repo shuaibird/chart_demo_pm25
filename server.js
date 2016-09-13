45c948aa44fa974b2224c512396bf59fdba6ff7a
Shuaibird <shuaibird@icloud.com>
2016-09-13 11:53:26 +0800

    server done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const PORT = 8080
 const HOST = 'http://119.29.166.125:8080'
+const DATA_API = '/dataapi/view/v2'
 
 var express = require('express')
 var app = express()
@@ -7,10 +8,23 @@ var request = require('superagent')
 
 app.use(express.static('public'))
 
-request
-  .get(`${HOST}/dataapi/view/v2/AVZa1KdFwe2Jxq0nEb01?data={"PRODUCT_KEY":"pk1","DID":"did1","FROM_TIMESTAMP_MILLIS":1473468218123,"TO_TIMESTAMP_MILLIS":1473557618123}`)
-  .end((err, res) => {
-    console.log(res.body)
+var apiRouter = express.Router()
+app.use(DATA_API, apiRouter)
+
+
+apiRouter
+  .get('/:view', (req, res, next) => {
+    var view = req.params.view
+
+    var dataQuery = JSON.stringify(req.query.data).replace(/\\/g, '')
+    dataQuery = dataQuery.slice(1, dataQuery.length - 1)
+
+    request
+      .get(`${HOST}${DATA_API}/${view}?data=${dataQuery}`)
+      .end((err, response) => {
+        res.json(response.body)
+      })
   })
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
+app.listen(PORT, () => console.log(`OPEN: http://localhost:${PORT}`))
